ba6b84f9a4761c8aac790c314b93e112e04c0974
Nadine Aisyah Putri Maharani <nadineaisyah170806@gmail.com>
2025-09-09 19:02:42 +0700

    Change font for html display

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'app_name': 'Goalified',
-        'name': 'Nadine Aisyah',
+        'name': '2406408224 - Nadine Aisyah Putri Maharani',
         'class_name': 'PBP F'
     }
     return render(request, 'main/main.html', context)
