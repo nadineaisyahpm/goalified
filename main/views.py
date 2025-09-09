ba9a315684b7bfb9ff7d3d713a05defa8a3e3998
Nadine Aisyah Putri Maharani <nadineaisyah170806@gmail.com>
2025-09-09 18:59:19 +0700

    Changing html display

--- main/views.py
+++ main/views.py
@@ -4,6 +4,6 @@ def show_main(request):
     context = {
         'app_name': 'Goalified',
         'name': 'Nadine Aisyah',
-        'class_name': 'Kelas PBP'
+        'class_name': 'PBP F'
     }
     return render(request, 'main/main.html', context)
